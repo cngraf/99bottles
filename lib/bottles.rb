f9cef052c2c1824678dc34511e01fdabedd40edf
Chris Graf <christopher.n.graf@gmail.com>
2018-03-22 10:28:09 -0500

    pass fourth test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Bottles
   class BottleVerse
     SPECIAL_STATES = [:nullular, :singular, :plural]
@@ -23,35 +25,41 @@ class Bottles
       @state = SPECIAL_STATES[n] || SPECIAL_STATES.last
     end
 
-    def bottle_form(n = @num)
-      NOUNS[:state] || NOUNS[:default] || n
+    def bottle_form
+      NOUNS[@state] || NOUNS[:default] || @num
     end
 
-    def count_word_form(n = @num)
-      COUNT_WORDS[:state] || COUNT_WORDS[:default] || n
+    def count_word_form
+      COUNT_WORDS[@state] || COUNT_WORDS[:default] || @num
     end
 
-    def pronoun_form(n = @num)
-      PRONOUNS[:state] || PRONOUNS[:default] || n
+    def pronoun_form
+      PRONOUNS[@state] || PRONOUNS[:default] || @num
     end
 
     def number_of_bottles
       "#{count_word_form} #{bottle_form}"
     end
 
-    def first_verse
+    def wall_phrase
       "#{number_of_bottles} of beer on the wall, #{number_of_bottles} of beer."
     end
 
-    def second_verse
-      "Take #{pronoun_form} down and pass it around, #{number_of_bottles} of beer on the wall."
+    def pass_phrase
+      "Take #{pronoun_form} down and pass it around,"
+    end
+
+    def leading_phrase
+      "#{number_of_bottles} of beer on the wall."
     end
   end
 
   def verse(n)
+this_verse = BottleVerse.new(n)
+next_verse = BottleVerse.new(n-1)
 <<-HEREDOC
-#{BottleVerse.new(n).first_verse}
-#{BottleVerse.new(n-1).second_verse}
+#{this_verse.wall_phrase}
+#{this_verse.pass_phrase} #{next_verse.leading_phrase}
 HEREDOC
   end
 end

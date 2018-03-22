3e0e394e69e4beb0d8be5a50734963416ea1b048
Chris Graf <christopher.n.graf@gmail.com>
2018-03-22 09:53:53 -0500

    pass third test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,8 +5,16 @@ class Bottles
 
   def get_verse(n)
     <<-VERSE
-#{n} bottles of beer on the wall, #{n} bottles of beer.
-Take one down and pass it around, #{n-1} bottles of beer on the wall.
+#{n} #{bottle_form(n)} of beer on the wall, #{n} #{bottle_form(n)} of beer.
+Take one down and pass it around, #{n-1} #{bottle_form(n-1)} of beer on the wall.
     VERSE
+  end
+
+  def bottle_form(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
 end

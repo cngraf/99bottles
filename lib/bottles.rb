c6e7012b7f5ee3d6279f5bf7904dc6d7a5ea87b4
Chris Graf <christopher.n.graf@gmail.com>
2018-03-29 18:43:19 -0500

    data-based approach

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,40 @@
 require 'pry'
+require 'pry'
 
 class Bottles
-  class BottleVerse
-    def initialize(n)
-      @num = n
+  class Wall
+    def initialize(number_of_bottles)
+      @number_of_bottles = number_of_bottles
     end
 
-    def bottle_form
-      case @num
+    def sing_a_verse!
+      first_count = count_the_bottles_of_beer
+
+      action_peformed = nil
+      if @number_of_bottles > 0
+        action_peformed = "take #{_pronoun} down and pass it around"
+        @number_of_bottles -= 1
+      else
+        action_peformed = "go to the store and buy some more"
+        @number_of_bottles = 99
+      end
+
+      recount = count_the_bottles_of_beer
+
+      <<~FULLROUND
+        #{first_count.capitalize} on the wall, #{first_count}.
+        #{action_peformed.capitalize}, #{recount} on the wall.
+      FULLROUND
+    end
+
+    def count_the_bottles_of_beer
+      "#{_number} #{_noun} of beer"
+    end
+
+    private
+
+    def _noun
+      case @number_of_bottles
       when 1
         "bottle"
       else
@@ -15,65 +42,37 @@ class Bottles
       end
     end
 
-    def count_word_form
-      case @num
+    def _number
+      case @number_of_bottles
       when 0
         "no more"
       else
-        @num
+        @number_of_bottles
       end
     end
 
-    def pronoun_form
-      case @num
+    def _pronoun
+      case @number_of_bottles
       when 1
         "it"
       else
         "one"
       end
     end
-
-    def next_verse
-      BottleVerse.new((@num - 1) % 100)
-    end
-
-    def number_of_bottles
-      "#{count_word_form} #{bottle_form}"
-    end
-
-    def first_line
-      "#{number_of_bottles.capitalize} of beer on the wall, #{number_of_bottles} of beer."
-    end
-
-    def second_line_start
-      case @num
-      when 0
-        "Go to the store and buy some more,"
-      else
-        "Take #{pronoun_form} down and pass it around,"
-      end
-    end
-
-    def second_line_end
-      "#{next_verse.number_of_bottles} of beer on the wall."
-    end
-
-    def full_verse
-      <<~HEREDOC
-      #{first_line}
-      #{second_line_start} #{second_line_end}
-      HEREDOC
-    end
   end
 
   def verse(n)
-    BottleVerse.new(n).full_verse
+    Wall.new(n).sing_a_verse!
   end
 
   def verses(start_num, end_num)
-    start_num.downto(end_num).map do |n|
-      BottleVerse.new(n).full_verse
-    end.join("\n")
+    song = []
+    wall = Wall.new(start_num)
+
+    (start_num - end_num + 1).times do
+      song << wall.sing_a_verse!
+    end
+    song.join("\n")
   end
 
   def song
